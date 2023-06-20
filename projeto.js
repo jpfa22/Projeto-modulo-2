246f9589db27600940db4232b582228f2f015196
João Alves <jpfa2@aluno.ifal.edu.br>
2023-06-20 17:49:44 -0300

    o script do projeto.js foi modificado

--- projeto.js
+++ projeto.js
@@ -1,117 +1,72 @@
+const assert = require('assert');
 
 // Simulação de dados de médicos
 const doctors = [
+  { id: 1, name: "Dr. João Silva", specialty: "Clínico Geral", availableDates: ["2023-06-18", "2023-06-19"], teleconsultation: true },
+  { id: 2, name: "Dra. Maria Souza", specialty: "Dermatologista", availableDates: ["2023-06-18", "2023-06-20"], teleconsultation: false },
+  { id: 3, name: "Dr. Carlos Santos", specialty: "Cardiologista", availableDates: ["2023-06-19", "2023-06-21"], teleconsultation: true }
+];
 
-    { id: 1, name: "Dr. João Silva", specialty: "Clínico Geral", availableDates: ["2023-06-18", "2023-06-19"], teleconsultation: true },
-    { id: 2, name: "Dra. Maria Souza", specialty: "Dermatologista", availableDates: ["2023-06-18", "2023-06-20"], teleconsultation: false },
-    { id: 3, name: "Dr. Carlos Santos", specialty: "Cardiologista", availableDates: ["2023-06-19", "2023-06-21"], teleconsultation: true }
-  ];
-  
-  // Função para exibir os resultados da pesquisa
-  function showSearchResults(results) {
-    const searchResults = document.getElementById("search-results");
-    searchResults.innerHTML = "";
-  
-    if (results.length === 0) {
-      searchResults.innerHTML = "<p>Nenhum médico encontrado.</p>";
-    } else {
-      results.forEach(doctor => {
-        const doctorCard = document.createElement("div");
-        doctorCard.classList.add("doctor-card");
-  
-        const doctorInfo = document.createElement("p");
-        doctorInfo.textContent = `${doctor.name} - ${doctor.specialty}`;
-  
-        const appointmentButton = document.createElement("button");
-        appointmentButton.textContent = "Marcar Consulta";
-        appointmentButton.addEventListener("click", () => {
-          showConfirmationArea(doctor);
-        });
-  
-        doctorCard.appendChild(doctorInfo);
-        doctorCard.appendChild(appointmentButton);
-        searchResults.appendChild(doctorCard);
-      });
-    }
-  }
-  
-  // Função para exibir a área de confirmação de consulta
-  function showConfirmationArea(doctor) {
-    const confirmationArea = document.getElementById("confirmation-area");
-    confirmationArea.innerHTML = "";
-  
-    const confirmationMessage = document.createElement("p");
-    confirmationMessage.textContent = `Você está prestes a marcar uma consulta com ${doctor.name}. Selecione uma data disponível:`;
-  
-    const dateSelect = document.createElement("select");
-    doctor.availableDates.forEach(date => {
-      const option = document.createElement("option");
-      option.value = date;
-      option.textContent = date;
-      dateSelect.appendChild(option);
-    });
-  
-    const confirmButton = document.createElement("button");
-    confirmButton.textContent = "Confirmar Consulta";
-    confirmButton.addEventListener("click", () => {
-      showAppointmentConfirmation(doctor, dateSelect.value);
-    });
-  
-    confirmationArea.appendChild(confirmationMessage);
-    confirmationArea.appendChild(dateSelect);
-    confirmationArea.appendChild(confirmButton);
-  }
-  
-  // Função para exibir a confirmação da consulta
-  function showAppointmentConfirmation(doctor, date) {
-    const confirmationArea = document.getElementById("confirmation-area");
-    confirmationArea.innerHTML = "";
-  
-    const successMessage = document.createElement("p");
-    successMessage.classList.add("message", "success");
-    successMessage.textContent = `Consulta marcada com sucesso!`;
-  
-    const prescriptionLink = document.createElement("a");
-    prescriptionLink.href = "caminho/para/prescricao.pdf";
-    prescriptionLink.textContent = "Baixar Prescrição";
-  
-    const teleconsultationMessage = document.createElement("p");
-    teleconsultationMessage.textContent = doctor.teleconsultation ? "Esta consulta será realizada por teleconsulta." : "";
-  
-    confirmationArea.appendChild(successMessage);
-    confirmationArea.appendChild(prescriptionLink);
-    confirmationArea.appendChild(teleconsultationMessage);
-  }
-  
-  // Evento de envio do formulário de cadastro
-  const registrationForm = document.getElementById("registration-form");
-  registrationForm.addEventListener("submit", (event) => {
-    event.preventDefault(); // Impede o comportamento padrão do envio do formulário
-  
-    const fullName = document.getElementById("full-name").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
-    const birthdate = document.getElementById("birthdate").value;
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    const language = document.getElementById("language").value;
-  
-  
-    // Exemplo de exibição de mensagem de sucesso
-    const registrationSuccessMessage = document.createElement("p");
-    registrationSuccessMessage.classList.add("message", "success");
-    registrationSuccessMessage.textContent = "Cadastro realizado com sucesso!";
-    registrationForm.appendChild(registrationSuccessMessage);
-  });
-  
-  // Evento de busca de médicos
-  const searchButton = document.getElementById("search-button");
-  searchButton.addEventListener("click", () => {
-    const searchInput = document.getElementById("search-input").value;
+// Função para exibir os resultados da pesquisa
+function showSearchResults(results) {
+  // Implementação da função de exibição dos resultados da pesquisa
+}
+
+// Função para exibir a área de confirmação de consulta
+function showConfirmationArea(doctor) {
+  // Implementação da função de exibição da área de confirmação de consulta
+}
+
+// Função para exibir a confirmação da consulta
+function showAppointmentConfirmation(doctor, date) {
+  // Implementação da função de exibição da confirmação da consulta
+}
+
+// Caso de teste 1: Busca por médico existente
+describe('Testes da API de Consultas Médicas', () => {
+  it('Deve retornar os resultados corretos ao buscar por médico existente', () => {
+    const searchInput = 'Dr. João';
     const results = doctors.filter(doctor =>
       doctor.name.toLowerCase().includes(searchInput.toLowerCase()) ||
       doctor.specialty.toLowerCase().includes(searchInput.toLowerCase())
     );
-    showSearchResults(results);
-  });
 
+    assert.strictEqual(results.length, 1);
+    assert.strictEqual(results[0].name, 'Dr. João Silva');
+  });
+});
+
+// Caso de teste 2: Busca por médico inexistente
+describe('Testes da API de Consultas Médicas', () => {
+  it('Deve retornar nenhum resultado ao buscar por médico inexistente', () => {
+    const searchInput = 'Dr. Anderson';
+    const results = doctors.filter(doctor =>
+      doctor.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+      doctor.specialty.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    assert.strictEqual(results.length, 0);
+  });
+});
+
+// Caso de teste 3: Exibição da área de confirmação de consulta
+describe('Testes da API de Consultas Médicas', () => {
+  it('Deve exibir a área de confirmação de consulta corretamente', () => {
+    const doctor = doctors[0];
+    const confirmationArea = document.createElement('div');
+    showConfirmationArea(doctor);
+
+    assert.strictEqual(confirmationArea.innerHTML.includes('Você está prestes a marcar uma consulta'), true);
+    assert.strictEqual(confirmationArea.getElementsByTagName('select').length, 1);
+    assert.strictEqual(confirmationArea.getElementsByTagName('button').length, 1);
+  });
+});
+
+// Caso de teste 4: Exibição da confirmação da consulta
+describe('Testes da API de Consultas Médicas', () => {
+  it('Deve exibir a confirmação da consulta corretamente', () => {
+    const doctor = doctors[0];
+    const date = doctor.availableDates[0];
+    const confirmationArea = document.createElement('div');
+  })
+});
